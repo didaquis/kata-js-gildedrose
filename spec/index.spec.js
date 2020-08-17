bc9191402b9742a0232a48b5340e49c005351316
Dídac García <didac.garcia@promocionesfarma.com>
2020-08-17 23:10:58 +0200

    Fix first test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,6 +4,6 @@ describe('Gilded Rose', () => {
 	it('should foo', () => {
 		const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
 		const items = gildedRose.updateQuality();
-		expect(items[0].name).toEqual('fixme');
+		expect(items[0].name).toEqual('foo');
 	});
 });

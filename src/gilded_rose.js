fef619e4efea53e280a892ef15483e72a387de65
Dídac García <didac.garcia@promocionesfarma.com>
2020-08-19 00:37:39 +0200

    Created a switch to separate logic by product type

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -26,11 +26,23 @@ class Shop {
 	updateQuality () {
 
 		this.items.forEach(item => {
+
+			switch (item.name) {
+				case this.AGED_BRIE:
+					break;
+				case this.BACKSTAGE_PASSES:
+					break;
+				case this.SULFURAS:
+					return;
+				case this.CONJURED:
+					break;
+				default:
+					break;
+			}
+
 			if (item.name != this.AGED_BRIE && item.name != this.BACKSTAGE_PASSES) {
 				if (item.quality > 0) {
-					if (item.name != this.SULFURAS) {
-						item.quality = this._decreaseByOne(item.quality);
-					}
+					item.quality = this._decreaseByOne(item.quality);
 				}
 			} else {
 				if (item.quality < 50) {
@@ -50,17 +62,13 @@ class Shop {
 				}
 			}
 
-			if (item.name != this.SULFURAS) {
-				item.sellIn = this._decreaseByOne(item.sellIn);
-			}
+			item.sellIn = this._decreaseByOne(item.sellIn);
 
 			if (item.sellIn < 0) {
 				if (item.name != this.AGED_BRIE) {
 					if (item.name != this.BACKSTAGE_PASSES) {
 						if (item.quality > 0) {
-							if (item.name != this.SULFURAS) {
-								item.quality = this._decreaseByOne(item.quality);
-							}
+							item.quality = this._decreaseByOne(item.quality);
 						}
 					} else {
 						item.quality = item.quality - item.quality;

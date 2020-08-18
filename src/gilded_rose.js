32b4d91aa30be6dfdd1490fe33f8d9912fc4b20a
Dídac García <didac.garcia@promocionesfarma.com>
2020-08-18 23:57:08 +0200

    Extracted the categories of products

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -9,19 +9,24 @@ class Item {
 class Shop {
 	constructor (items = []){
 		this.items = items;
+		this.AGED_BRIE = 'Aged Brie';
+		this.BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+		this.SULFURAS = 'Sulfuras, Hand of Ragnaros';
+		this.CONJURED = 'Conjured Mana Cake';
 	}
+
 	updateQuality () {
-		for (var i = 0; i < this.items.length; i++) {
-			if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+		for (let i = 0; i < this.items.length; i++) {
+			if (this.items[i].name != this.AGED_BRIE && this.items[i].name != this.BACKSTAGE_PASSES) {
 				if (this.items[i].quality > 0) {
-					if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+					if (this.items[i].name != this.SULFURAS) {
 						this.items[i].quality = this.items[i].quality - 1;
 					}
 				}
 			} else {
 				if (this.items[i].quality < 50) {
 					this.items[i].quality = this.items[i].quality + 1;
-					if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+					if (this.items[i].name == this.BACKSTAGE_PASSES) {
 						if (this.items[i].sellIn < 11) {
 							if (this.items[i].quality < 50) {
 								this.items[i].quality = this.items[i].quality + 1;
@@ -35,14 +40,16 @@ class Shop {
 					}
 				}
 			}
-			if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+
+			if (this.items[i].name != this.SULFURAS) {
 				this.items[i].sellIn = this.items[i].sellIn - 1;
 			}
+
 			if (this.items[i].sellIn < 0) {
-				if (this.items[i].name != 'Aged Brie') {
-					if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+				if (this.items[i].name != this.AGED_BRIE) {
+					if (this.items[i].name != this.BACKSTAGE_PASSES) {
 						if (this.items[i].quality > 0) {
-							if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+							if (this.items[i].name != this.SULFURAS) {
 								this.items[i].quality = this.items[i].quality - 1;
 							}
 						}
